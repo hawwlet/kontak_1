4797f4db923d8d9b445f0652607d742c5ae79643
hawwlet <hari.rofalon.geous@gmail.com>
2021-04-26 17:55:17 +0800

    soal ke 3

---
+++ soal3.py
@@ -0,0 +1,10 @@
+mylist = []
+
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[ Daftar kontak ]) # prints 1
+print(mylist[ Tambah kontak ]) # prints 2
+print(mylist[ Keluar ]) # prints 3
+
+print(mylist)
